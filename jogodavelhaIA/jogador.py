e9c47d537c3ca2905d4ca526cb00ab75ad65c0d0
Fred Martins <143145370+fredmartins12@users.noreply.github.com>
2025-04-22 13:46:30 -0300

    Update comentarios jogador.py

--- jogodavelhaIA/jogador.py
+++ jogodavelhaIA/jogador.py
@@ -1,13 +1,12 @@
-# -*- coding: utf-8 -*-
-
-from tabuleiro import Tabuleiro
-
 class Jogador:
-    def __init__(self, tabuleiro : Tabuleiro, tipo : int):
-        self.matriz = tabuleiro.matriz
-        self.tabuleiro = tabuleiro
-        self.tipo = tipo
-        
-      
-    def getJogada(self) -> (int, int):
-        pass
+    """
+    Classe base para jogadores (Humanos ou IA).
+    """
+    def __init__(self, simbolo):
+        self.simbolo = simbolo  # Armazena 'X' ou 'O'
+
+    def jogar(self, tabuleiro):
+        """
+        Método a ser implementado pelas subclasses para fazer a jogada.
+        """
+        raise NotImplementedError("Método jogar deve ser implementado pelas subclasses")

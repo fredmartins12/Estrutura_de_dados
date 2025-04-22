b92dfc45eba62f8e7a7a1ad22495b09fb6335c97
Fred Martins <143145370+fredmartins12@users.noreply.github.com>
2025-04-22 14:12:47 -0300

    Update main.py

--- jogodavelhaIA/main.py
+++ jogodavelhaIA/main.py
@@ -1,18 +1,7 @@
-# Arquivo: main.py
-# -*- coding: utf-8 -*-
-import pygame
-from jogo_velha import JogoDaVelha
-from tabuleiro_screen import TelaTabuleiro
+from jogo_velha import JogoVelha
+    
+jogo = JogoVelha()
 
-# Inicialização do Pygame
-pygame.init()
-# Cria janela 600x600 pixels
-tela = pygame.display.set_mode((600, 600))
-pygame.display.set_caption("Jogo da Velha IA")
+jogo.start()
 
-# Cria instâncias do jogo e da tela
-jogo = JogoDaVelha()
-tela_jogo = TelaTabuleiro(tela, jogo)
-# Inicia o loop de eventos e desenho da interface
-tela_jogo.loop()
-
+jogo.wait_quit_event()

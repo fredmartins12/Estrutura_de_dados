22285d69b2b4d9ee68c856d830420bf032c77fcb
Fred Martins <143145370+fredmartins12@users.noreply.github.com>
2025-04-22 14:13:34 -0300

    Update tabuleiro.py

--- jogodavelhaIA/tabuleiro.py
+++ jogodavelhaIA/tabuleiro.py
@@ -1,43 +1,36 @@
 class Tabuleiro:
-    """
-    Representa o estado do tabuleiro (3x3):
-    VAZIO = 0, X = 1, O = 2
-    """
-    VAZIO = 0
-    X = 1
-    O = 2
+    DESCONHECIDO = 0
+    JOGADOR_0 = 1
+    JOGADOR_X = 4
+    OUTRO_TIPO = 2
 
     def __init__(self):
-        # Cria matriz 3x3 vazia
-        self.matriz = [[Tabuleiro.VAZIO]*3 for _ in range(3)]
+        self.matriz = [ [Tabuleiro.DESCONHECIDO, Tabuleiro.DESCONHECIDO, Tabuleiro.DESCONHECIDO], 
+                        [Tabuleiro.DESCONHECIDO, Tabuleiro.DESCONHECIDO, Tabuleiro.DESCONHECIDO],
+                        [Tabuleiro.DESCONHECIDO, Tabuleiro.DESCONHECIDO, Tabuleiro.DESCONHECIDO]]
 
-    def esta_livre(self, linha, coluna):
-        # Retorna True se posição estiver vazia
-        return self.matriz[linha][coluna] == Tabuleiro.VAZIO
-
-    def marcar(self, linha, coluna, simbolo):
-        # Marca X ou O se estiver livre
-        if self.esta_livre(linha, coluna):
-            self.matriz[linha][coluna] = simbolo
-            return True
-        return False
-
-    def cheia(self):
-        # Retorna True se não houver espaço vazio
-        return all(self.matriz[i][j] != Tabuleiro.VAZIO
-                   for i in range(3) for j in range(3))
-
-    def verificar_vitoria(self, simbolo):
-        m = self.matriz
-        # Linhas e colunas
+    def tem_campeao(self):
+        # Verificar linhas
         for i in range(3):
-            if all(m[i][j] == simbolo for j in range(3)):
-                return True
-            if all(m[j][i] == simbolo for j in range(3)):
-                return True
-        # Diagonais
-        if m[0][0] == simbolo == m[1][1] == m[2][2]:
-            return True
-        if m[0][2] == simbolo == m[1][1] == m[2][0]:
-            return True
-        return False
+            if self.matriz[i][0] == self.matriz[i][1] == self.matriz[i][2] != Tabuleiro.DESCONHECIDO:
+                return self.matriz[i][0]
+        
+        # Verificar colunas
+        for i in range(3):
+            if self.matriz[0][i] == self.matriz[1][i] == self.matriz[2][i] != Tabuleiro.DESCONHECIDO:
+                return self.matriz[0][i]
+        
+        # Verificar diagonais
+        if self.matriz[0][0] == self.matriz[1][1] == self.matriz[2][2] != Tabuleiro.DESCONHECIDO:
+            return self.matriz[0][0]
+        if self.matriz[0][2] == self.matriz[1][1] == self.matriz[2][0] != Tabuleiro.DESCONHECIDO:
+            return self.matriz[0][2]
+        
+        # Verifica se há empate
+        for i in range(3):
+            for j in range(3):
+                if self.matriz[i][j] == Tabuleiro.DESCONHECIDO:
+                    return Tabuleiro.DESCONHECIDO
+        
+        # Empate
+        return Tabuleiro.OUTRO_TIPO
